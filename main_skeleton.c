cfed5c141e79ddf89772d63ae258c8adaf0c252d
WungusCode <joubu.tim@gmail.com>
2025-09-10 13:02:00 -0700

    main_skeleton.c
        Create bare bones window, titled 'window'

--- main_skeleton.c
+++ main_skeleton.c
@@ -8,16 +8,40 @@
 
 static app_flags flgs;
 
+static void destroy( GtkWidget *widget,    gpointer   data );
+
 int main(int argc, char* argv[]) {
   int rc = 0;
 
   rc = parse_cmdline( argc, argv , &flgs );
   if ( rc == 0 ) {
-    gtk_init(&argc, &argv);
+    GtkWidget* window;
+    hdl_grp   all_hdls;
+    phdl_grp  pall_hdls = NULL;
+
+    pall_hdls = &all_hdls;
+
+    pall_hdls->flg = &flgs;
+
+		gtk_init(&argc, &argv);
 
     // code
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    pall_hdls->parentWin = window;
+
+    gtk_window_set_title (GTK_WINDOW (window), "Window");
+    gtk_window_set_default_size (GTK_WINDOW (window), WIN_W, WIN_H );
+
+    if ( pall_hdls->flg->dbg ) {
+      printf( "  window = %p \n" , window );
+    }
+    g_signal_connect (G_OBJECT (window), "destroy", G_CALLBACK (destroy), NULL);
 
     gtk_main();  // blocks until GTK terminates
   }
   return rc;
+}
+
+static void destroy( GtkWidget *widget, gpointer   data ) {
+  gtk_main_quit ();
 }
