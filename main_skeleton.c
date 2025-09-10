82901c6d150bbbc98afd4385b5fbae53b97f74f6
WungusCode <joubu.tim@gmail.com>
2025-09-10 12:52:51 -0700

    main_skeleton.c
        update to use new 'parse_cmdline' API. Only call gtk code if parse_cmdline() returns 0

--- main_skeleton.c
+++ main_skeleton.c
@@ -2,14 +2,22 @@
 #include<gtk/gtk.h>
 
 #include "base_defs.h"
+#include "data_types.h"
+
+#include "params.h"
+
+static app_flags flgs;
 
 int main(int argc, char* argv[]) {
   int rc = 0;
 
-  gtk_init(&argc, &argv);
+  rc = parse_cmdline( argc, argv , &flgs );
+  if ( rc == 0 ) {
+    gtk_init(&argc, &argv);
 
-  // code
+    // code
 
-  gtk_main();  // blocks until GTK terminates
+    gtk_main();  // blocks until GTK terminates
+  }
   return rc;
 }
