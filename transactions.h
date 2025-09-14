f7f08b8c54a7e2dd9d2a96d8c15aae527c75800d
WungusCode <joubu.tim@gmail.com>
2025-09-13 18:35:44 -0700

    transactions.[ch]
        add transactions.*, to add/del transactions from okane_grp linked list

---
+++ transactions.h
@@ -0,0 +1,8 @@
+#ifndef _TRANSACTIONS_H
+
+#define _TRANSACTIONS_H 1
+
+extern int del_transaction ( pokane_grp *t_lst , pokane_grp data_in, int dbg );
+extern int add_transaction ( pokane_grp *t_lst , pokane_grp data_in, int dbg );
+
+#endif
