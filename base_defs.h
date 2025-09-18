bbae2dae0e3295104c5d1ed232b2dc654567eaa8
Dylanht341 <112240916+Dylanht341@users.noreply.github.com>
2025-09-18 01:01:59 -0700

    Makefile needs to be revised

--- base_defs.h
+++ base_defs.h
@@ -6,14 +6,20 @@
 #include <gmodule.h>
 
 #include "data_types.h"
-
+//Window Size
 #define WIN_W 720
 #define WIN_H 480
 
-// keep at bottom
+// Shared handle passed between screens
 typedef struct _hdl_grp {
-  GtkWidget *parentWin;
+  GtkWidget *parentWin;  // main window (set in main.c)
 
+  // Screen containers (created on first visit, re-used later)
+  GtkWidget *vbox_one;   // Start Screen (was "screen_one")
+  GtkWidget *vbox_two;   // Login Screen
+  GtkWidget *vbox_three; // Create Account Screen
+  GtkWidget *vbox_four;  // Home Page
+  
   papp_flags flg;
 } hdl_grp, *phdl_grp;
 
