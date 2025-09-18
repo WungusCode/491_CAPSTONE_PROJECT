d8c9515930433c438370e18d31d28c7ab7d40c76
Dylanht341 <112240916+Dylanht341@users.noreply.github.com>
2025-09-18 01:05:57 -0700

    Makefile needs to be revised

--- home_page.c
+++ home_page.c
@@ -3,28 +3,39 @@
 
 #include "base_defs.h"
 
+int start_dbg = 0;
 
-int create_home_screen ( phdl_grp all_hdls ) {
+int create_home_screen ( phdl_grp pall_hdls ) {
 
   GtkWidget *vbox, *hbox , *hbox2 , *hbox3, *hbox_spc;
   GtkWidget *label, *blnk_label;
   GtkWidget *button;
 
-  int rc = 0;
+  // int rc = 0;
 
-  printf( "  >> E %s \n" , __FUNCTION__ );
+  // printf( "  >> E %s \n" , __FUNCTION__ );
 
-  if ( all_hdls->flg->dbg ) {
-    printf( "  >> E %s \n" , __FUNCTION__ );
-    printf( "    flgs->dbg = %d \n" , all_hdls->flg->dbg );
-    printf( "    parentWin = %p \n" , all_hdls->parentWin );
-  }
+  // if ( pall_hdls->flg->dbg ) {
+  //   printf( "  >> E %s \n" , __FUNCTION__ );
+  //   printf( "    flgs->dbg = %d \n" , pall_hdls->flg->dbg );
+  //   printf( "    parentWin = %p \n" , pall_hdls->parentWin );
+  // }
   //actions = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
 
-  vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
-  gtk_container_add (GTK_CONTAINER ( all_hdls->parentWin ), vbox);
+  if ( pall_hdls == NULL ) {
+    printf( "  >> E %s  , all_hdls = NULL !!! \n" , __FUNCTION__ );
+    return -1;
+  }
 
-  printf( "    vbox = %p \n" , vbox );
+  if ( start_dbg ) {
+    printf( "  >> E %s \n" , __FUNCTION__ );
+    printf( "    parentWin = %p \n" , pall_hdls->parentWin );
+  }
+  
+  pall_hdls->vbox_four = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
+  gtk_container_add (GTK_CONTAINER ( pall_hdls->parentWin ), pall_hdls->vbox_four);
+
+  printf( "    vbox = %p \n" , pall_hdls->vbox_four );
 
   hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 12);
 
@@ -34,7 +45,7 @@ int create_home_screen ( phdl_grp all_hdls ) {
 
   gtk_widget_show_all (hbox);
 
-  gtk_container_add (GTK_CONTAINER ( vbox ), hbox );
+  gtk_container_add (GTK_CONTAINER ( pall_hdls->vbox_four ), hbox );
 
   hbox2 = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 12);
 
@@ -43,19 +54,19 @@ int create_home_screen ( phdl_grp all_hdls ) {
   gtk_widget_set_valign ( button, GTK_ALIGN_CENTER);
   gtk_box_pack_start (GTK_BOX ( hbox2 ), button, TRUE, FALSE, 0);
 
-  gtk_container_add (GTK_CONTAINER ( vbox ), hbox2 );
+  gtk_container_add (GTK_CONTAINER ( pall_hdls->vbox_four ), hbox2 );
 
   hbox_spc   = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 12);
   blnk_label = gtk_label_new ("");
   gtk_box_pack_start (GTK_BOX (hbox_spc), blnk_label, TRUE, FALSE, 0);
 
-  gtk_container_add (GTK_CONTAINER ( vbox ), hbox_spc );
+  gtk_container_add (GTK_CONTAINER ( pall_hdls->vbox_four ), hbox_spc );
 
   hbox_spc   = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 12);
   blnk_label = gtk_label_new ("");
   gtk_box_pack_start (GTK_BOX (hbox_spc), blnk_label, TRUE, FALSE, 0);
 
-  gtk_container_add (GTK_CONTAINER ( vbox ), hbox_spc );
+  gtk_container_add (GTK_CONTAINER ( pall_hdls->vbox_four ), hbox_spc );
 
   hbox3 = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 12);
 
@@ -71,9 +82,9 @@ int create_home_screen ( phdl_grp all_hdls ) {
 
   gtk_box_pack_start (GTK_BOX ( hbox3 ), button, TRUE, FALSE, 0);
 
-  gtk_container_add (GTK_CONTAINER ( vbox ), hbox3 );
+  gtk_container_add (GTK_CONTAINER ( pall_hdls->vbox_four ), hbox3 );
 
-  gtk_widget_show_all ( vbox );
+  gtk_widget_show_all ( pall_hdls->vbox_four );
 
   if ( all_hdls->flg->dbg ) {
     printf( "  << Lv %s \n" , __FUNCTION__ );
@@ -81,5 +92,5 @@ int create_home_screen ( phdl_grp all_hdls ) {
 
   printf( "  << Lv %s \n" , __FUNCTION__ );
 
-  return rc;
+  return 0;
 }
