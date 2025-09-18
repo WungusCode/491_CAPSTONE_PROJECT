5c9c00eec98dc063066fe97295bcb4a1e2e6a745
Rafael482 <rafaelmercado@csu.fullerton.edu>
2025-09-17 19:10:36 -0700

    worked on  email, password and started reporting a problem

--- setting_page.c
+++ setting_page.c
@@ -1,28 +1,156 @@
+#include <stdio.h>
 #include <gtk/gtk.h>
-#include "setting_page.h"
+#include "settings_page.h"
 #include "base_defs.h"
 
-int setting_page_creation(void){
-  int rc = 0;
-  //create a new window called settings 
-  gtkWidget *window;
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_title(GTK_WINDOW(window), "Settings");
+// --- Callbacks ---
 
-  printf("Settings page created\n");
-
-  # create the notfication box
-  GtkWidget *check = gtk_check_button_new_with_label("Enable Notifications");
-  gtk_container_add(GTK_CONTAINER(window), check);
-
-  gtk_widget_show_all(window);
-
-  printf(""<< Lv %s\n"", __function__);
-  return rc;
+static void on_close_clicked(GtkButton *btn, gpointer user_data) {
+    GtkWidget *window = GTK_WIDGET(user_data);
+    if (window) gtk_widget_destroy(window);
 }
 
- void destory_setting_page(void){
-   printf("Settings page destroyed\n");
- } 
-  
+static void on_notify_toggled(GtkToggleButton *toggle, gpointer user_data) {
+    gboolean active = gtk_toggle_button_get_active(toggle);
+    g_print("[Settings] Notifications: %s\n", active ? "ON" : "OFF");
+}
+
+static void apply_dark_preference(gboolean prefer_dark) {
+    GtkSettings *settings = gtk_settings_get_default();
+    if (settings) {
+        g_object_set(settings, "gtk-application-prefer-dark-theme", prefer_dark, NULL);
+        g_print("[Settings] Theme set to %s mode\n", prefer_dark ? "Dark" : "Light");
+    } else {
+        g_warning("[Settings] Could not get GtkSettings to set theme.");
+    }
+}
+
+static void on_theme_toggled(GtkToggleButton *toggle, gpointer user_data) {
+    // We wire this only to the "Dark" radio; when it becomes active, set dark.
+    if (gtk_toggle_button_get_active(toggle)) {
+        apply_dark_preference(TRUE);
+    }
+}
+
+static void on_theme_light_clicked(GtkToggleButton *toggle, gpointer user_data) {
+    if (gtk_toggle_button_get_active(toggle)) {
+        apply_dark_preference(FALSE);
+    }
+}
+
+static void on_reportProblem_clicked(GtkButton *btn, gpointer user_data) {
+    g_print("[Settings] Report Problem clicked\n");
+    // Placeholder for actual report problem functionality
+    GtkWidget *report_dialog = gtk_message_dialog_new(GTK_WINDOW(user_data),
+                                                      GTK_DIALOG_MODAL,
+                                                      GTK_MESSAGE_INFO,
+                                                      GTK_BUTTONS_OK,
+                                                      "How to report problems here");
+    gtk_dialog_run(GTK_DIALOG(report_dialog));
+    gtk_widget_destroy(report_dialog);
+}
+
+// --- Public API ---
+
+GtkWidget* create_setting_page(void) {
+    // New Settings window
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Settings");
+    gtk_window_set_default_size(GTK_WINDOW(window), WIN_W / 2, WIN_H / 2);
+
+    // Root layout
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 16);
+    gtk_container_set_border_width(GTK_CONTAINER(vbox), 16);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    // ===== Notifications =====
+    GtkWidget *frame_notify = gtk_frame_new("Notifications");
+    GtkWidget *box_notify = gtk_box_new(GTK_ORIENTATION_VERTICAL, 8);
+    gtk_container_set_border_width(GTK_CONTAINER(box_notify), 8);
+    gtk_container_add(GTK_CONTAINER(frame_notify), box_notify);
+
+    // add to main box
+    gtk_box_pack_start(GTK_BOX(vbox), frame_login, FALSE, FALSE, 0);
+
+    GtkWidget *check_notify = gtk_check_button_new_with_label("Enable Notifications");
+    g_signal_connect(check_notify, "toggled", G_CALLBACK(on_notify_toggled), NULL);
+    gtk_box_pack_start(GTK_BOX(box_notify), check_notify, FALSE, FALSE, 0);
+
+    gtk_box_pack_start(GTK_BOX(vbox), frame_notify, FALSE, FALSE, 0);
+
+    // ===== Appearance (Light / Dark) =====
+    GtkWidget *frame_theme = gtk_frame_new("Appearance");
+    GtkWidget *box_theme = gtk_box_new(GTK_ORIENTATION_VERTICAL, 8);
+    gtk_container_set_border_width(GTK_CONTAINER(box_theme), 8);
+    gtk_container_add(GTK_CONTAINER(frame_theme), box_theme);
+
+    // Radio buttons: Light / Dark
+    GtkWidget *radio_light = gtk_radio_button_new_with_label(NULL, "Light mode");
+    GtkWidget *radio_dark  = gtk_radio_button_new_with_label_from_widget(GTK_RADIO_BUTTON(radio_light), "Dark mode");
+
+    // Connect both so you can switch either way
+    g_signal_connect(radio_dark,  "toggled", G_CALLBACK(on_theme_toggled), NULL);
+    g_signal_connect(radio_light, "toggled", G_CALLBACK(on_theme_light_clicked), NULL);
+
+    // Initialize radios based on current setting
+    GtkSettings *settings = gtk_settings_get_default();
+    gboolean prefer_dark = FALSE;
+    if (settings) {
+        g_object_get(settings, "gtk-application-prefer-dark-theme", &prefer_dark, NULL);
+    }
+    if (prefer_dark) {
+        gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(radio_dark), TRUE);
+    } else {
+        gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(radio_light), TRUE);
+    }
+
+    gtk_box_pack_start(GTK_BOX(box_theme), radio_light, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(box_theme), radio_dark,  FALSE, FALSE, 0);
+
+    gtk_box_pack_start(GTK_BOX(vbox), frame_theme, FALSE, FALSE, 0);
+
+    // Account information email + password 
+    GtkWidget *frame_account = gtk_frame_new("Account");
+    GtkWdiget *box_account = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    Gtk_container_set_border_width(GTK_CONTAINER(box_account), 10);
+    Gtk_container_add(GTK_CONTAINER(frame_account), box_account);
+
+    // where the Email will be entered
+    GtkWidget *enter_email = gtk_enter_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(enter_email), "Email");
+    gtk_box_pack_start(GTK_BOX(box_account), enter_email, FALSE, FALSE, 0);
+
+    //eneter the password box 
+    GtkWidget *enter_password = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(enter_password), "Password");
+    //  Makes sure that the password is visible so that you know what you typed in.
+    // if somone gets into the account it's likely the already know the password is there
+    // not point in hiding it
+    gtk_entry_set_visibility(GTK_ENTRY(enter_password), TRUE); 
+    gtk_box_pack_start(GTK_BOX(box_account), enter_password, FALSE, FALSE, 0)
+
+    // add to main box
+    gtk_box_pack_start(GTK_BOX(vbox), frame_account, FALSE, FALSE, 0);
+
+
+
+
+    // Close button at bottom
+    GtkWidget *btn_close = gtk_button_new_with_label("Close");
+    g_signal_connect(btn_close, "clicked", G_CALLBACK(on_close_clicked), window);
+    gtk_box_pack_end(GTK_BOX(vbox), btn_close, FALSE, FALSE, 0);
+
+    // Show
+    gtk_widget_show_all(window);
+
+    printf("[Settings] %s\n", __FUNCTION__);
+    return window;
+}
+
+void destroy_setting_page(GtkWidget *window) {
+    if (window) {
+        gtk_widget_destroy(window);
+    }
+    printf("[Settings] window destroyed\n");
+}
 
