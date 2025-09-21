3f5b0c4601c757c9a7ba42b75b187cae0ab488f9
lanayam <lanayam@csu.fullerton.edu>
2025-09-21 12:31:43 -0700

    Added Pie Page Files

---
+++ pie_page.h
@@ -0,0 +1,11 @@
+#ifndef _PIE_PAGE_H
+
+#define _PIE_PAGE_H 1
+
+#include "base_defs.h"
+#include "data_types.h"
+
+extern int create_pie_chart_page    ( phdl_grp pall_hdls );
+extern int create_pie_chart_page_rtn( phdl_grp *all_hdls );
+
+#endif
