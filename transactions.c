ac6eed594f860a13c8564bc6582adf346b54435d
WungusCode <joubu.tim@gmail.com>
2025-09-24 13:50:13 -0700

    transactions.c
        update to use new database format 'category'

--- transactions.c
+++ transactions.c
@@ -23,7 +23,7 @@ int create_dummy_data( pokane_grp *head ) {
   lst_len       = get_okane_grp_list_len( *head );
   if ( dbg ) printf( "      list len = %3d  \n" , lst_len );
 
-  tmp.is_income = 1;
+  tmp.category  = INCOME_ID;
   tmp.entry_nr  = lst_len;
   tmp.entry_ts  = 0;
   tmp.amount    = 100.0;
@@ -32,7 +32,7 @@ int create_dummy_data( pokane_grp *head ) {
   add_transaction( head , &tmp , 1 );
   lst_len++;
 
-  tmp.is_income = 0;
+  tmp.category  = TRANSPORT_ID;
   tmp.entry_nr  = lst_len;
   tmp.entry_ts  = 0;
   tmp.amount    = 20.0;
@@ -41,7 +41,7 @@ int create_dummy_data( pokane_grp *head ) {
   add_transaction( head , &tmp , 1 );
   lst_len++;
 
-  tmp.is_income = 0;
+  tmp.category  = FOOD_ID;
   tmp.entry_nr  = lst_len;
   tmp.entry_ts  = 0;
   tmp.amount    = 40.0;
@@ -50,7 +50,7 @@ int create_dummy_data( pokane_grp *head ) {
   add_transaction( head , &tmp , 1 );
   lst_len++;
 
-  tmp.is_income = 0;
+  tmp.category  = HOUSING_ID;
   tmp.entry_nr  = lst_len;
   tmp.entry_ts  = 0;
   tmp.amount    = 30.0;
@@ -59,7 +59,7 @@ int create_dummy_data( pokane_grp *head ) {
   add_transaction( head , &tmp , 1 );
   lst_len++;
 
-  tmp.is_income = 0;
+  tmp.category  = WORK_ID;
   tmp.entry_nr  = lst_len;
   tmp.entry_ts  = 0;
   tmp.amount    = 15.0;
@@ -142,7 +142,7 @@ int add_transaction ( pokane_grp *t_lst , pokane_grp data_in, int dbg ) {
   tmp = malloc( sizeof( okane_grp ) );
   tmp->next      = NULL;
   tmp->entry_nr  = data_in->entry_nr;    // TODO auto generate this by counting entries in list so far
-  tmp->is_income = data_in->is_income;
+  tmp->category = data_in->category;
   tmp->entry_ts  = get_unix_time_now();
   tmp->amount    = data_in->amount;
   tmp->in_dB     = data_in->in_dB;
