b8f39ac5937805b4984ea401202cb851e4db1415
WungusCode <joubu.tim@gmail.com>
2025-09-24 18:29:53 -0700

    main_skeleton.c
        update to use 'cp_myPie' and initialize it to null

--- main_skeleton.c
+++ main_skeleton.c
@@ -11,8 +11,8 @@
 #include "login_screen.h"
 #include "home_page.h"
 #include "transact_page.h"
-#include "custom_pie_widget.h"
 #include "setting_page.h"
+#include "custom_pie_widget.h"
 #include "pie_page.h"
 
 #include "database/my_sql.h"
@@ -21,9 +21,7 @@
 
 static app_flags flgs;
 
-static void destroy( GtkWidget *widget,    gpointer   data ) {
-  gtk_main_quit ();
-}
+static void destroy( GtkWidget *widget,    gpointer   data );
 
 int main(int argc, char* argv[]) {
   int rc = 0;
@@ -55,18 +53,19 @@ int main(int argc, char* argv[]) {
 
     pall_hdls->vbx_hdls = malloc ( sizeof ( uiHdl ) );
     printf( "  pall_hdls->vbx_hdls = %p \n" , pall_hdls->vbx_hdls );  // RM
+    memset( pall_hdls->vbx_hdls, 0 , sizeof ( uiHdl ) );
+    printf( "  pall_hdls->vbx_hdls = %p \n" , pall_hdls->vbx_hdls ); // RM
 
     pall_hdls->vbx_hdls->hp_chart_btn      = NULL;
     pall_hdls->vbx_hdls->hp_plus_trans_btn = NULL;
     pall_hdls->vbx_hdls->hp_list_trans_btn = NULL;
-    pall_hdls->vbx_hdls->thp_xx = NULL;
 
     pall_hdls->vbx_hdls->tp_w_is_income   = NULL;
     pall_hdls->vbx_hdls->tp_w_amount      = NULL;
     pall_hdls->vbx_hdls->tp_w_description = NULL;
 
-    pall_hdls->vbx_hdls->thp_xx = NULL;
-    pall_hdls->vbx_hdls->cp_xx  = NULL;
+    pall_hdls->vbx_hdls->thp_xx    = NULL;
+    pall_hdls->vbx_hdls->cp_myPie  = NULL;
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     pall_hdls->parentWin = window;
@@ -74,11 +73,14 @@ int main(int argc, char* argv[]) {
     gtk_window_set_title (GTK_WINDOW (window), "Window");
     gtk_window_set_default_size (GTK_WINDOW (window), WIN_W, WIN_H );
 
+    if ( pall_hdls->flg->dbg ) {
+      printf( "  window = %p \n" , window );
+    }
     g_signal_connect (G_OBJECT (window), "destroy", G_CALLBACK (destroy), NULL);
 
     get_data_from_db ( &pall_hdls );
     printf("    pall_hdls->t_list = %p , %s , L%4d \n", pall_hdls->t_lst, __func__, __LINE__);
-
+ 
     create_start_screen_rtn             (  &pall_hdls );
     create_login_screen_rtn             (  &pall_hdls );
     create_create_screen_rtn            (  &pall_hdls );
@@ -89,12 +91,13 @@ int main(int argc, char* argv[]) {
     printf( "  pall_hdls = %p pall_hdls->flg = %p pall_hdls->flg->dbg = %d \n" , pall_hdls, pall_hdls->flg, pall_hdls->flg->dbg );
     create_transaction_history_page_rtn (  &pall_hdls );
 
-    //set_all_hdls_from ( &all_hdls, __FUNCTION__ , __LINE__  );
-  
-
     gtk_widget_show_all ( window );
 
     gtk_main();  // blocks until GTK terminates
   }
   return rc;
+}
+
+static void destroy( GtkWidget *widget, gpointer   data ) {
+  gtk_main_quit ();
 }
