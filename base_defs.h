af7cbcd8ea27a6f2247c2a4e14ca839ca1bc3d6a
WungusCode <joubu.tim@gmail.com>
2025-09-25 14:38:14 -0700

    base_defs.h
        add pointers for auto_test

--- base_defs.h
+++ base_defs.h
@@ -43,8 +43,13 @@ typedef struct _okane_grp {
 typedef struct _uiHdl {
   // home page widgets
   GtkWidget *hp_plus_trans_btn;
+  GtkWidget *hp_plus_trans_done_btn;
+
   GtkWidget *hp_chart_btn;
+  GtkWidget *hp_chart_done_btn;
+
   GtkWidget *hp_list_trans_btn;
+  GtkWidget *hp_list_trans_done_btn;
 
   // transaction_page widgets
   GtkWidget *tp_w_is_income;
